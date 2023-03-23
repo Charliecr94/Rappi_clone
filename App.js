de0637e8eee48d3d3c143cc0c4b6a7d2d38781f9
Carlos Smith <93277919+Charliecr94@users.noreply.github.com>
2023-03-22 20:41:05 -0600

    Implementing the paper-searchbar

--- App.js
+++ App.js
@@ -1,20 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { Searchbar } from "react-native-paper";
+import { StatusBar, StyleSheet, SafeAreaView, Text, View } from "react-native";
+
+import { RestaurantsScreen } from "./src/components/test";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <SafeAreaView style={styles.container}>
+        <View style={styles.search}>
+          <Searchbar />
+        </View>
+        <View style={styles.list}>
+          <Text> xddd </Text>
+        </View>
+      </SafeAreaView>
+      <RestaurantsScreen style={styles.container} />
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: StatusBar.currentHeight,
+    padding: 0,
+    flexDirection: "column",
+    backgroundColor: "black",
+  },
+  search: {
+    backgroundColor: "white",
+    padding: 24,
+  },
+  list: {
+    flex: 1,
+    backgroundColor: "blue",
+    padding: 16,
   },
 });
