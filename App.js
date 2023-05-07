f092e82c7ef322104c3ad2c20c0f411114775351
Carlos Smith <93277919+Charliecr94@users.noreply.github.com>
2023-05-07 14:11:19 -0600

    Faking API request

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
 import React from "react";
 import { ThemeProvider } from "styled-components/native";
-import { Text } from "react-native"
+import { Text } from "react-native";
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import Ionicons from "@expo/vector-icons";
+import { Ionicons } from "@expo/vector-icons";
 
-import {
+import {  
   useFonts as useOswald,
   Oswald_400Regular,
 } from "@expo-google-fonts/oswald";
@@ -16,20 +16,36 @@ import { theme } from "./src/infrastructure/theme";
 import { RestaurantsScreen } from "./src/features/restaurants/screens/restaurants.screen";
 import { SafeArea } from "./src/components/utility/safe-area.component";
 
+import { restaurantsRequest } from "./src/services/restaurants/mock/restaurants.service";
+
 const Tab = createBottomTabNavigator();
+
+const TAB_ICON = {
+  Restaurants: "md-restaurant",
+  Map: "md-map",
+  Settings: "md-settings",
+};
 
 const Settings = () => (
   <SafeArea>
-    <Text> Settings </Text>
+    <Text>Settings</Text>
   </SafeArea>
 );
 const Map = () => (
   <SafeArea>
-    <Text> Map </Text>
+    <Text>Map</Text>
   </SafeArea>
 );
 
 
+const createScreenOptions = ({ route }) => {
+  const iconName = TAB_ICON[route.name];
+  return {
+    tabBarIcon: ({ size, color }) => (
+      <Ionicons name={iconName} size={size} color={color}/>
+    ),
+  };
+};
 
 export default function App() {
   const [oswaldLoaded] = useOswald({
@@ -49,22 +65,7 @@ export default function App() {
       <ThemeProvider theme={theme}>
         <NavigationContainer>
           <Tab.Navigator
-            screenOptions={({ route }) => ({
-              tabBarIcon: ({ color, size }) => {
-                let iconName;
-
-                if (route.name === "Restaurants") {
-                  iconName = "md-restaurant";
-                } else if (route.name === "Settings") {
-                  iconName = "md-settings";
-                } else if (route.name === "Map") {
-                  iconName = "md-map";
-                }
-
-                // You can return any component that you like here!
-                return <Ionicons name={iconName} size={size} color={color} />;
-              },
-            })}
+            screenOptions={createScreenOptions}
             tabBarOptions={{
               activeTintColor: "tomato",
               inactiveTintColor: "gray",
